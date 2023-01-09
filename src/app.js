80f3ba09a4a55b4c7da2de970646e29c25a0646d
AnaLauraFFerraz <analaurafferraz@gmail.com>
2023-01-09 17:02:01 -0300

    fix tweets storage

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.post("/sign-up", (req, res) => {
     }
 
     users.push({ username, avatar });
-    
+
     res.status(201).send("OK!");
 });
 
@@ -40,27 +40,35 @@ app.post("/tweets", (req, res) => {
         return;
     }
 
-    const tweet = {
-        username: newTweet.username,
-        avatar: userData.avatar,
-        tweet: newTweet.tweet
-    }
-    tweets.push(tweet);
-
     if (!newTweet.username || !newTweet.tweet) {
         res.status(400).send("Todos os campos são obrigatórios!");
         return;
     }
 
+    tweets.push(newTweet);
+
     res.status(201).send("OK!");
 });
 
 app.get("/tweets", (req, res) => {
+    let lastTweets = []
+    let userData
+
+    tweets.map(tweet => {
+        userData = users.find(user => user.username === tweet.username);
+
+        lastTweets.push({
+            username: tweet.username,
+            avatar: userData.avatar,
+            tweet: tweet.tweet
+        })
+    })
 
     let listSize = 0;
-    if (tweets.length >= 10) listSize = tweets.length - 10;
 
-    const lastTweets = tweets.slice(listSize).reverse();
+    if (lastTweets.length >= 10) listSize = tweets.length - 10;
+
+    lastTweets = lastTweets.slice(listSize).reverse();
 
     res.status(200).send(lastTweets);
 });
